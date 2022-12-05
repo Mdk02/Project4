9a51c020232e0450fe4788e27420dfd43087d396
Sharik <sharik2002.sharipov@yandex.ru>
2022-12-06 00:44:30 +0500

    join G B A 06.12

---
+++ Project/монитор.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="/catalog-sidebar.php"/>
+    <title>Document</title>
+</head>
+<body>
+
+    <form action=product-details-sticky-right.php method=”post”>
+        
+        <input type=”text” name=”some_field” value=””>
+        <input type="submit" value=”Отправить”>
+    </form>
+ 
+</body>
+</html>
+
+
